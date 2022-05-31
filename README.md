5735f2fca6a808fc8e29837e2b3e9fca727bb548
Richard Chao <richardchao2009@gmail.com>
2022-05-31 12:36:54 +1000

    added main file and src directory

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Sudoku
+# Sudoku Solver Project
+
+
+# Sudoku solver adapted from:
+https://www.youtube.com/watch?v=mcXc8Mva2bA&list=PLvNXocZO6_pjSMdilYjV4Wzll5zLKHcsa&index=1&t=190s
---
+++ src/SudokuSolver.java
@@ -0,0 +1,41 @@
+public class SudokuSolver {
+
+    private static final int GRID_SIZE = 9;
+
+
+    public static void main(String[] args) {
+
+        int[][] board = {
+                {7, 0, 2, 0, 5, 0, 6, 0, 0},
+                {0, 0, 0, 0, 0, 3, 0, 0, 0},
+                {1, 0, 0, 0, 0, 9, 5, 0, 0},
+                {8, 0, 0, 0, 0, 0, 0, 9, 0},
+                {0, 4, 3, 0, 0, 0, 7, 5, 0},
+                {0, 9, 0, 0, 0, 0, 0, 0, 8},
+                {0, 0, 9, 7, 0, 0, 0, 0, 5},
+                {0, 0, 0, 2, 0, 0, 0, 0, 0},
+                {0, 0, 7, 0, 4, 0, 2, 0, 3},
+        };
+    }
+
+    private static boolean isNumberInRow(int[][] board, int number, int row) {
+        for (int i = 0; i < GRID_SIZE; i++) {
+            if (board[row][i] == number) {
+                return true;
+            }
+        }
+        return false;
+    };
+
+    private static boolean isNumberInColumn(int[][] board, int number, int column) {
+        for (int i = 0; i < GRID_SIZE; i++) {
+            if (board[i][column] == number) {
+                return true;
+            }
+        }
+        return false;
+
+    };
+
+}
+
